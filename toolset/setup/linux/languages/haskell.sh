dd6d3e3cf97e3c8c668ce88cd4ea207eb21cf5f2
Nathan Smyth <nathan.andrew.smyth@gmail.com>
2015-09-05 16:05:24 +1000

    Updating Haskell build tools
    Now at the latest versions as of 05/09/15.

--- toolset/setup/linux/languages/haskell.sh
+++ toolset/setup/linux/languages/haskell.sh
@@ -4,4 +4,4 @@ export LANG=en_US.UTF-8
 
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install -y ghc-7.8.3 cabal-install-1.20 libpcre3-dev
+sudo apt-get install -y ghc-7.10.3 cabal-install-1.22 libpcre3-dev
